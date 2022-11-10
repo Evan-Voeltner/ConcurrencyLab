c78cc4adce2d68d6cec51979dda608aee35a54cb
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-10 12:54:59 -0600

    feat: withdraw method locked with synchronized keyword + run() turned into a lambda function

--- src/Simulation.java
+++ src/Simulation.java
@@ -4,13 +4,14 @@ public class Simulation {
         System.out.println("Running simulation");
         Account myAccount = new Account(1000);
 
-        Runnable runWithdraw = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    myAccount.withdraw(100);
+        Runnable runWithdraw = () -> {
+                for (int i = 0; i < 1000; i++) {
+                    try {
+                        myAccount.withdraw(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }
         };
 
         Thread thread1 = new Thread(runWithdraw, "Thread 1");
